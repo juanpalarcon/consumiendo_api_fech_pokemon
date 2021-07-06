f6c217d2975e7410d8350680489ea0e4d6417e4b
Juan Pablo <juanpablogast@gmail.com>
2021-07-06 11:39:23 -0400

    starting function masPokemones

--- pokemon.js
+++ pokemon.js
@@ -1,26 +1,27 @@
 $(document).ready(function () {
-  $("#mas").click(function () {
-    $("#name").html("");
-    $.ajax({
-      type: "get",
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      url: "https://pokeapi.co/api/v2/pokemon/",
-      data: "data",
+  $("#name").html("");
+  $("#url").html("");
+  $.ajax({
+    type: "get",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    url: "https://pokeapi.co/api/v2/pokemon/",
+    data: "data",
 
-      success: function (data) {
-        console.log(data.results);
-        $.each(data.results, function (i, item) {
-          $("#name").html($("#name").html() + ` <ul>${item.name}</ul> `);
-          $("#url").html($("#url").html() + ` <ul>${item.url}</ul> `);
-        });
-      },
-      error: function (xhr, status, error) {
-        console.log(xhr);
-        console.log(status);
-        console.log(error);
-      },
-    });
+    success: function (data) {
+      console.log(data.results);
+      $.each(data.results, function (i, item) {
+        $("#name").html($("#name").html() + ` <ul>${item.name}</ul> `);
+        $("#url").html($("#url").html() + ` <ul>${item.url}</ul> `);
+      });
+
+      function masPokemones() {}
+    },
+    error: function (xhr, status, error) {
+      console.log(xhr);
+      console.log(status);
+      console.log(error);
+    },
   });
 });
 
