51a74a0c215b68bdfd979e9ef3d23df9d73de4b9
Juan Pablo <juanpablogast@gmail.com>
2021-07-05 22:57:04 -0400

    First commit

---
+++ pokemon.js
@@ -0,0 +1,28 @@
+document.querySelector("#body").addEventListener("click", traerDatos());
+
+function traerDatos() {
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://pokeapi.co/api/v2/pokemon/", true);
+
+  request.send();
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      respuesta = JSON.parse(this.responseText);
+
+      //   console.log(respuesta.results);
+
+      let name = document.querySelector("#name");
+      name.innerHTML = "";
+
+      let characteristic = document.querySelector("#characteristic");
+      characteristic.innerHTML = "";
+
+      for (let item of respuesta.results) {
+        name.innerHTML += ` <ul>${item.name}</ul> `;
+        characteristic.innerHTML += ` <ul><a href= "${item.url}" ></a></ul> ${item.url}`;
+      }
+
+      //   var nombre = document.querySelector("#nombre-pokemon");
+    }
+  };
+}
